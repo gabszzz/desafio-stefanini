2331ccbdf1f087d8c2f8a809807ed9808ea1bb26
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2022-09-13 23:49:04 -0300

    initial commit

--- backend/Repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package Desafio.AlunoDisciplina.Repository;
-
-
-import Desafio.AlunoDisciplina.Entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
-
-}
---
+++ backend/src/main/java/com/desafio/backend/entity/Aluno.java
@@ -0,0 +1,5 @@
+package com.desafio.backend.entity;
+
+public class Aluno {
+    
+}
